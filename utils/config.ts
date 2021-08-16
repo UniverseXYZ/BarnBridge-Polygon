d32fccefbe9e1d03f65f21973df582b93028a9e3
Alex T <alex@tzapu.com>
2021-08-17 00:32:30 +0300

    testnet deployments

--- utils/config.ts
+++ utils/config.ts
@@ -25,7 +25,7 @@ export function config(hre: HardhatRuntimeEnvironment): BBNetConfig {
   const cfg = {
     "hardhat": {
       path: "deploy-testnet",
-      seed: "dev06",
+      seed: "xyz01",
       tokenAddress: "0xd7d55Fd7763A356aF99f17C9d6c21d933bC2e2F1",
       rootChainManager: "0xBbD7cBFA79faee899Eaf900F13C9065bF03B1A74",
       erc20Predicate: "0xdD6596F2029e6233DEFfaCa316e6A95217d4Dc34",
@@ -40,7 +40,7 @@ export function config(hre: HardhatRuntimeEnvironment): BBNetConfig {
     },
     "goerli": {
       path: "deploy-goerli",
-      seed: "dev06",
+      seed: "xyz02",
       tokenAddress: "0xd7d55Fd7763A356aF99f17C9d6c21d933bC2e2F1",
       rootChainManager: "0xBbD7cBFA79faee899Eaf900F13C9065bF03B1A74",
       erc20Predicate: "0xdD6596F2029e6233DEFfaCa316e6A95217d4Dc34",
@@ -53,7 +53,7 @@ export function config(hre: HardhatRuntimeEnvironment): BBNetConfig {
     },
     "mumbai": {
       path: "deploy-mumbai",
-      seed: "dev06",
+      seed: "xyz02",
       tokenAddress: "0xebB83c1b86A27eb9e2523A2c117F1d656269dbAE",
       rootChainManager: addressZero,
       erc20Predicate: addressZero,
